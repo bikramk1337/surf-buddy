a0ed7f5e30017509b18bfab6d5374048bf190c07
Bikram <bikramk1337@gmail.com>
2025-01-27 19:00:58 +1100

    feat: add custom icon, update popup component, and setup global styles

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,20 +1,17 @@
 import { useState } from "react"
+import sbIcon from "data-base64:../assets/icon.png"
+import "styles/popup.css"
 
 function IndexPopup() {
   const [data, setData] = useState("")
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h1>
-        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
-      </h1>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <footer>Crafted by @PlasmoHQ</footer>
+    <div>
+      <div className="header">
+        <img src={sbIcon} alt="SurfBuddy"/>
+        <h1>SurfBuddy</h1>
+      </div>
+      <p>Summarize search results and chat with Ollama models locally.</p>
     </div>
   )
 }
